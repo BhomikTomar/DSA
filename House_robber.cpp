2cc6aafd10e790aa3edd46f059f8fbe93643536e
BhomikTomar <bhomiktomar@gmail.com>
2024-09-22 16:20:33 +0530

    Initial commit: Added house_robber C++ solution

--- House_robber.cpp
+++ House_robber.cpp
@@ -1,34 +1,8 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-// int f(vector<int>& arr,int index,int n,vector<int>& dp){
-//     if(index == 0) return arr[index];
-//     if(index<0) return 0;
-//     if(dp[index] != -1) return dp[index];
-
-//     int pick = arr[index] + f(arr,index-2,n,dp);
-//     int notPick = f(arr,index-1,n,dp);
-
-//     return dp[index] = max(pick,notPick);
-// }
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        // vector<int> dp(n,-1);
-
-        vector<int> nums(n);
-
-        for(int i=0;i<n;i++){
-            cin>>nums[i];
-        }
-
-        // return f(arr,n-1,n,dp);
-
-        int prev = nums[0];
+int f(vector<int>& nums,int n){
+    int prev = nums[0];
         int prev2 = 0;
         for(int i=1;i<n;i++){
             int pick = nums[i];
@@ -39,6 +13,22 @@ int main()
             prev = curi;
         }
         return prev;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        vector<int> nums(n);
+
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+        }
+
+        cout<<f(nums,n)<<endl;
     }
     return 0;
 }
