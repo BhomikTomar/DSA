d1478b2afd73c5db624cf5f6aeb9d9dd27299a05
Bhomik Tomar <bhomiktomar@gmail.com>
2025-08-18 12:25:40 +0530

    dfs detection cycle

--- graphs/cycle_detection_undirected_dfs.cpp
+++ graphs/cycle_detection_undirected_dfs.cpp
@@ -1,3 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool dfs(int node, int parent, vector<int> &visited, vector<vector<int>> &adj) {
+    visited[node] = 1;
+    for(int adjNode : adj[node]) {
+        if(!visited[adjNode]) {
+            dfs(adjNode, node, visited, adj);
+        } else if(adjNode != parent) {
+            return true; 
+        }   
+    }
+    return false; 
+}
+bool isCyclic(int n, vector<vector<int>> &adj) {
+    vector<int> visited(n, 0);  
+    
+    for(int i = 0; i < n; i++) {
+        if(!visited[i]) {
+            if(dfs(i, -1, visited, adj)) {
+                return true; 
+            }
+        }
+    }
+    return false; 
+}
