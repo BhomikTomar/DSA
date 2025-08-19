4922366089896da46565a5755e1fb412836b4eef
Bhomik Tomar <bhomiktomar@gmail.com>
2025-08-19 23:18:11 +0530

    dsfsd

--- graphs/cycle_detection_undirected_dfs.cpp
+++ graphs/cycle_detection_undirected_dfs.cpp
@@ -12,9 +12,10 @@ bool dfs(int node, int parent, vector<int> &visited, vector<vector<int>> &adj) {
     }
     return false; 
 }
+
 bool isCyclic(int n, vector<vector<int>> &adj) {
     vector<int> visited(n, 0);  
-    
+
     for(int i = 0; i < n; i++) {
         if(!visited[i]) {
             if(dfs(i, -1, visited, adj)) {
