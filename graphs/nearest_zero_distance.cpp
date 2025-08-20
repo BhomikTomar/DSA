f805d9bb65bd6b6b47c33e6c64b0223628884fb4
Bhomik Tomar <bhomiktomar@gmail.com>
2025-08-20 13:03:23 +0530

    nearest_zero_distance cpp soln

---
+++ graphs/nearest_zero_distance.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vector<vector<int>> dist(m, vector<int>(n, 0));
+        queue<pair<pair<int, int>, int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            dist[row][col] = steps;
+
+            for(int i = 0; i < 4; i++){
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && vis[nr][nc] != 1){
+                    vis[nr][nc] = 1;
+                    q.push({{nr, nc}, steps + 1});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
