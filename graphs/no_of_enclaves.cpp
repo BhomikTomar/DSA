947fea397cc16a7cef0fa6f1e2a1a716093f0406
Bhomik Tomar <bhomiktomar@gmail.com>
2025-08-25 15:46:45 +0530

    added surrounding islands cpp soln

--- graphs/no_of_enclaves.cpp
+++ graphs/no_of_enclaves.cpp
@@ -1,52 +1,57 @@
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, vector<vector<int>>& vis, int row, int col, int dr[], int dc[]) {
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int row, int col, int dr[], int dc[]){
         vis[row][col] = 1;
-        int m = board.size();
-        int n = board[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        for (int i = 0; i < 4; i++) {
+        for(int i = 0; i < 4; i++){
             int nr = row + dr[i];
             int nc = col + dc[i];
 
-            if (nr >= 0 && nc >= 0 && nr < m && nc < n 
-                && vis[nr][nc] == 0 && board[nr][nc] == 'O') {
-                dfs(board, vis, nr, nc, dr, dc);
+            if(nr < m && nc < n && nr >=0 && nc >=0 && vis[nr][nc] == 0 && grid[nr][nc] == 1){
+                dfs(grid, vis, nr, nc, dr, dc);
             }
         }
     }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
 
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        if (m == 0) return;
-        int n = board[0].size();
-
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O' && vis[i][0] == 0)
-                dfs(board, vis, i, 0, dr, dc);
-            if (board[i][n - 1] == 'O' && vis[i][n - 1] == 0)
-                dfs(board, vis, i, n - 1, dr, dc);
-        }
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O' && vis[0][j] == 0)
-                dfs(board, vis, 0, j, dr, dc);
-            if (board[m - 1][j] == 'O' && vis[m - 1][j] == 0)
-                dfs(board, vis, m - 1, j, dr, dc);
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O' && vis[i][j] == 0)
-                    board[i][j] = 'X';
+        for(int i = 0; i < m; i++){
+            if(grid[i][0] == 1 && vis[i][0] == 0){
+                dfs(grid, vis, i, 0, dr, dc);
+            }
+            if(grid[i][n - 1] == 1 && vis[i][n - 1] == 0){
+                dfs(grid, vis, i, n - 1, dr, dc);
             }
         }
+
+        for(int i = 0; i < n; i++){
+            if(grid[0][i] == 1 && vis[0][i] == 0){
+                dfs(grid, vis, 0, i, dr, dc);
+            }
+            if(grid[m - 1][i] == 1 && vis[m - 1][i] == 0){
+                dfs(grid, vis, m - 1, i, dr, dc);
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(vis[i][j] == 0 && grid[i][j] == 1) count++;
+            }
+        }
+
+        return count;
     }
 };
